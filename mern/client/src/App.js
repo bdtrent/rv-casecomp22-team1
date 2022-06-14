324681b17796a247260bf97400757e209e9e37e5
rv-jupagan <jupagan@redventures.com>
2022-06-14 19:59:22 -0400

    added homepage to app.js

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -8,6 +8,7 @@ import Navbar from "./components/navbar";
 import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
+import Homepage from "./components/homepage";
  
 const App = () => {
  return (
@@ -17,7 +18,7 @@ const App = () => {
        <Route exact path="/" element={<RecordList />} />
        <Route path="/edit/:id" element={<Edit />} />
        <Route path="/create" element={<Create />} />
-       
+       <Route path="/" element={<Homepage />}/>
      </Routes>
    </div>
  );
--- mern/client/src/components/homepage.js
+++ mern/client/src/components/homepage.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import App from "../App";
 
-export default function startQuiz(){
+export default function Homepage(){
     return(
         <div>
             <button>Start Quiz!</button>
