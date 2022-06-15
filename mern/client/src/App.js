8343c9b76e5d46f995366a14bb6dd59fc4a019ee
rv-jupagan <jupagan@redventures.com>
2022-06-14 21:54:32 -0400

    Substituted recordlist to quiz selection component

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -5,7 +5,6 @@ import { Route, Routes } from "react-router-dom";
  
 // We import all the components we need in our app
 import Navbar from "./components/navbar";
-import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
 import Homepage from "./components/homepage";
@@ -15,10 +14,10 @@ const App = () => {
    <div>
      <Navbar />
      <Routes>
-       <Route exact path="/" element={<RecordList />} />
+       <Route path="/" element={<Homepage />}/>
+
        <Route path="/edit/:id" element={<Edit />} />
        <Route path="/create" element={<Create />} />
-       <Route exact path="/" element={<Homepage />}/>
      </Routes>
    </div>
  );
