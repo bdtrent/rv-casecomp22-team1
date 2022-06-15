9df9010da60e0edecec7d4dba3f66f55a052fab8
Ancil Trent <trent12@purdue.edu>
2022-06-15 15:52:44 -0400

    Rewrote quiz to better receive backend data, not fully functioning

--- mern/client/src/components/quiz.js
+++ mern/client/src/components/quiz.js
@@ -1,57 +1,77 @@
 import React, {useState} from 'react';
 
+const ProgressBar = ({
+    currentQuestion, questionCount
+}) => {
+    const progressPercentage = (currentQuestion / questionCount) * 100
+
+    return <div className='progressBar'>
+        <div className='text'>
+            {currentQuestion} answered ({questionCount - currentQuestion} remaining)
+        </div>
+        <div className='inner' style={{width: `${progressPercentage}%`}} />
+    </div>
+}
+
 export default function Quiz() {
     // Hard coded quiz object, replace with quiz pulled from DB
     const questions = [
         {
             questionText: 'Which of the following movies was NOT directed by Alfred Hitchcock?',
             answerOptions: [
-                { answerText: 'The Birds', isCorrect: false},
-                { answerText: "The Omen", isCorrect: true},
-                {answerText: 'Psycho', isCorrect: false},
-                {answerText:'Vertigo', isCorrect: false}
-            ]
+                { answerText: 'The Birds'},
+                { answerText: "The Omen"},
+                { answerText: 'Psycho'},
+                { answerText: 'Vertigo'}
+            ],
+            correctIndex: 2
         },
         {
             questionText: 'Which of the following movies does NOT have a sequel?',
             answerOptions: [
-                {answerText: 'Insidious', isCorrect:false},
-                {answerText: 'Scream', isCorrect:false},
-                {answerText: 'Shutter Island', isCorrect:true},
-                {answerText: 'The Conjuring', isCorrect:false}
-            ]
+                {answerText: 'Insidious'},
+                {answerText: 'Scream'},
+                {answerText: 'Shutter Island'},
+                {answerText: 'The Conjuring'}
+            ],
+            correctIndex: 3
         },
         {
             questionText: 'What is the name of the evil doll in Child\'s Play?',
             answerOptions: [
-                {answerText: 'Charlie', isCorrect:false},
-                {answerText:'Chucky', isCorrect:true},
-                {answerText:'Chastity', isCorrect:false},
-                {answerText:'Chase', isCorrect:false}
-            ]
+                {answerText: 'Charlie'},
+                {answerText:'Chucky'},
+                {answerText:'Chastity'},
+                {answerText:'Chase'}
+            ],
+            correctIndex: 2
         },
         {
             questionText: 'In which movie do the killers wear fox, tiger, and lamb masks?',
             answerOptions: [
-                {answerText: 'Pet Semetary', isCorrect:false},
-                {answerText: 'Friday the 13th', isCorrect:false},
-                {answerText: 'A Nightmare on Elm Street', isCorrect:false},
-                {answerText: 'You\'re Next', isCorrect:true}
-            ]
+                {answerText: 'Pet Semetary'},
+                {answerText: 'Friday the 13th'},
+                {answerText: 'A Nightmare on Elm Street'},
+                {answerText: 'You\'re Next'}
+            ],
+            correctIndex: 4
         },
         {
             questionText: 'Which film is directed by Ari Aster?',
             answerOptions: [
-                {answerText: 'The Shining', isCorrect:false},
-                {answerText: 'Hereditary', isCorrect:true},
-                {answerText: 'Sharknado', isCorrect:false},
-                {answerText:'My Bloody Valentine', isCorrect:false}
-            ]
+                {answerText: 'The Shining'},
+                {answerText: 'Hereditary'},
+                {answerText: 'Sharknado'},
+                {answerText:'My Bloody Valentine'}
+            ],
+            correctIndex: 2
         }
     ];
 
     // State values for quiz game
     const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [correctAnswer, setCorrectAnswer] = useState(null);
+    const [answerStatus, setAnswerStatus] = useState(null);
     const [showScore, setShowScore] = useState(false);
     const [score, setScore] = useState(0);
 
@@ -61,15 +81,20 @@ export default function Quiz() {
         if (isCorrect) {
             setScore(score + 1);
         }
+        let thisQuestion = questions[currentQuestion];
+        setCorrectAnswer(thisQuestion.answerOptions[thisQuestion.correctIndex].answerText);
+        setAnswerStatus(isCorrect);
+    }
 
+    const onNextClick = () => { 
         // Move on to next question if there is one, otherwise show results
-        const nextQuestion = currentQuestion + 1;
-        if (nextQuestion < questions.length) {
-            setCurrentQuestion(nextQuestion);
-        } else {
+        if (currentQuestion+1 === questions.length) {
             setShowScore(true);
+        } else {
+            setCurrentQuestion(currentQuestion+1)
+            setAnswerStatus(null);
         }
-    };
+    }
 
     return (
         <div className='quiz'>
@@ -79,19 +104,27 @@ export default function Quiz() {
                 </div>
             ): (
                 <>
+                    <ProgressBar currentQuestion={currentQuestion} questionCount={questions.length} />
                     <div className='question'>
-                        <div className='question-count'>
-                            <span>Question {currentQuestion+1}</span>/{questions.length}
-                        </div>
                         <div className='question-text'>
                             {questions[currentQuestion].questionText}
                         </div>
                     </div>
                     <div className='answers'>
-                        {questions[currentQuestion].answerOptions.map((answerOption) => (
-                            <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+                        {questions[currentQuestion].answerOptions.map((answerOption, index) => (
+                            <button onClick={() => handleAnswerOptionClick(index === questions[currentQuestion].correctIndex)}>{answerOption.answerText}</button>
                         ))}
                     </div>
+                    {answerStatus != null && (
+                        <div>
+                            <div className='answerResult'>
+                                {answerStatus ? "Correct!" : "Incorrect, the correct answer was: " + {correctAnswer}}
+                                <button className='next' onClick={onNextClick}>
+                                    {currentQuestion === questions.length - 1 ? "See results" : "Next Question"}
+                                </button>
+                            </div>
+                        </div>
+                    )}
                 </>
             )}
         </div>
