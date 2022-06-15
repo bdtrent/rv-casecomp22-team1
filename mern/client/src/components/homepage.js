f432bd8cbc59f6e71281cff19949303960cd0642
rv-jupagan <jupagan@redventures.com>
2022-06-15 13:12:29 -0400

    Horror and General Buttons

--- mern/client/src/components/homepage.js
+++ mern/client/src/components/homepage.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const quizzes = [{
     name: "Horror", 
-    name: "General" 
-    }]
+    name: "General"
+}]
 export default function Homepage(){
     return(
         <div>
             <h2 id="homeTitle">Choose Your Trivia!</h2>
             <div id="initialSelection">
                 <button>Horror Genre Quiz</button>
-                <button>Random Genre Quiz!</button>
+                <button>General Genre Quiz!</button>
             </div>
         </div>
     );
