1df312f0651c03a29c66b9566d3f5e20aaa960e5
rv-jupagan <jupagan@redventures.com>
2022-06-15 09:02:39 -0400

    removed Create button

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -5,8 +5,6 @@ import { Route, Routes } from "react-router-dom";
  
 // We import all the components we need in our app
 import Navbar from "./components/navbar";
-import Edit from "./components/edit";
-import Create from "./components/create";
 import Homepage from "./components/homepage";
 
 import "./App.css";
--- mern/client/src/components/navbar.js
+++ mern/client/src/components/navbar.js
@@ -25,16 +25,8 @@ export default function Navbar() {
        >
          <span className="navbar-toggler-icon"></span>
        </button>
- 
-       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-         <ul className="navbar-nav ml-auto">
-           <li className="nav-item">
-             <NavLink className="nav-link" to="/create">
-               Create Record
-             </NavLink>
-           </li>
-         </ul>
-       </div>
+
+
      </nav>
    </div>
  );
