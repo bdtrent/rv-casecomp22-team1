c4cb76afc0382312608fe1fa33546df5e1d3ca11
rv-jupagan <jupagan@redventures.com>
2022-06-16 10:05:28 -0400

    dynamic columns

--- mern/client/src/components/homepage.js
+++ mern/client/src/components/homepage.js
@@ -1,29 +1,32 @@
 import React from "react";
 import { Card, Button, Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const quizzes = [
     { id: 1, name: "Horror"},
-    { id: 2, name: "General"}
+    { id: 2, name: "Rom-Com"}
 ];
 export default function Homepage(){
     return(
         <div>
             <h2 id="homeTitle">Choose Your Trivia!</h2>
             <Container>
-                <Row md={4}>
+                <Row md={quizzes.length}>
                     {quizzes.map(quizzes => {
                         return(
                             <Col key={quizzes.id}>
                                 <Card style={{ width: '18rem' }}>
-                                    <Card.Img variant="top" src="holder.js/100px180" />
-                                    <Card.Body>
-                                        <Card.Title>Start {quizzes.name} Genre Quiz</Card.Title>
-                                        <Card.Text>
-                                            
-                                        </Card.Text>
-                                        <Button variant="primary">Start {quizzes.name}</Button>
-                                    </Card.Body>
-                                </Card>    
+                                <Card.Img variant="top" src="assets/Horror.png/100px180" />
+                                <Card.Body>
+                                    <Card.Title>{quizzes.name} Trivia</Card.Title>
+                                    <Card.Text>
+                                   
+                                    </Card.Text>
+                                    <Link to="./quiz">
+                                        <Button variant="primary">Start Quiz</Button>
+                                    </Link>
+                                </Card.Body>
+                                </Card>
                             </Col>
                         )
                     })}
