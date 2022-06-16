f3d151746b485d8b381fc972192a19fabb4354d4
rv-jupagan <jupagan@redventures.com>
2022-06-16 08:59:06 -0400

    react-bootstrap container + cards

--- mern/client/src/components/homepage.js
+++ mern/client/src/components/homepage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
 
 const quizzes = [
     { id: 1, name: "Horror"},
@@ -9,13 +9,26 @@ export default function Homepage(){
     return(
         <div>
             <h2 id="homeTitle">Choose Your Trivia!</h2>
-            <div id="initialSelection">
-                {quizzes.map(quizzes => {
-                    return(
-                        <Button key={quizzes.id}>Start {quizzes.name} Genre Quiz</Button>
-                    )
-                })}
-            </div>
+            <Container>
+                <Row md={4}>
+                    {quizzes.map(quizzes => {
+                        return(
+                            <Col key={quizzes.id}>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src="holder.js/100px180" />
+                                    <Card.Body>
+                                        <Card.Title>Start {quizzes.name} Genre Quiz</Card.Title>
+                                        <Card.Text>
+                                            
+                                        </Card.Text>
+                                        <Button variant="primary">Start {quizzes.name}</Button>
+                                    </Card.Body>
+                                </Card>    
+                            </Col>
+                        )
+                    })}
+                </Row>
+            </Container>
         </div>
     );
 }
