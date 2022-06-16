b47d87de16293b7c5babf7029836d5742b17b18c
Yunah Kim <kimy19@wfu.edu>
2022-06-16 11:10:10 -0400

    Updated conn.js with new RomCom Quiz

--- mern/server/db/conn.js
+++ mern/server/db/conn.js
@@ -37,7 +37,7 @@ module.exports = {
                 { QuizQuestion: {
                   isMulti: false,
                   id:  'tt0094862',
-                  question: 'In what year was the movie Child’s Play released?',
+                  question: 'In what year was the movie "Child’s Play" released?',
                   answers: [{answerText: '1988'}, {answerText: '1995'} 
                   ,{answerText: '1980'}, {answerText: '1977'}],
                   correctIndex: 0
@@ -119,6 +119,108 @@ module.exports = {
               ]
             };
             _db.collection('Quizzes').insertOne(quiz, function(err, res) {
+              if (err) throw err
+            });
+
+            let quiz2 = {
+              quizName: 'RomComQuiz',
+              
+              quizQuestions: [
+                  
+                { QuizQuestion: {
+                    isMulti: true,
+                    id:  '',
+                    question: 'Joseph Gordon-Levitt and Zooey Deschanel starred in which of the following rom-com movies?',
+                    answers: [{answerText: 'tt3152624'}, {answerText: 'tt1022603'} 
+                    ,{answerText: 'tt1486834'}, {answerText: 'tt1195478'}],
+                    correctIndex: 1
+                  }
+                },
+                { QuizQuestion: {
+                  isMulti: false,
+                  id:  'tt0098635',
+                  question: 'In “When Harry Met Sally,” what was the name of the song Harry and Sally were singing when they ran into Harry’s ex-wife, Helen?',
+                  answers: [{answerText: 'Surrey With a Fringe on Top'}, {answerText: 'Oh What a Beautiful Mornin’'} 
+                  ,{answerText: 'I’m Just a Girl Who Can’t Say No'}, {answerText: 'Poor Judd is Dead'}],
+                  correctIndex: 0
+                  }
+                },
+                { QuizQuestion: {
+                  isMulti: true,
+                  id:  '',
+                  question: 'Rebel Wilson and Liam Hemsworth starred in which of the following movies?',
+                  answers: [{answerText: 'tt1872818'}, {answerText: 'tt2194499'} 
+                  ,{answerText: 'tt0415978'}, {answerText: 'tt2452244'}],
+                  correctIndex: 3
+                  }
+                },
+                { QuizQuestion: {
+                  isMulti: false,
+                  id:  'tt0108160',
+                  question: 'In the beginning of “Sleepless in Seattle,” Tom Hanks moves to Seattle from what city?',
+                  answers: [{answerText: 'San Francisco'}, {answerText: 'Boston'} 
+                  ,{answerText: 'Chicago'}, {answerText: 'Brooklyn'}],
+                  correctIndex: 2
+                  }
+                },
+                { QuizQuestion: {
+                  isMulti: true,
+                  id:  '',
+                  question: 'Which of the following rom-com movies were NOT directed by Amy Heckerling?',
+                  answers: [{answerText: 'tt0408839'}, {answerText: 'tt0466839'} 
+                  ,{answerText: 'tt0112697'}, {answerText: 'tt0217630'}],
+                  correctIndex: 0
+                  }
+                },
+                { QuizQuestion: {
+                  isMulti: false,
+                  id:  'tt0147800',
+                  question: 'Which Shakespeare play is “10 Things I Hate About You” based on?',
+                  answers: [{answerText: 'The Winter’s Tale'}, {answerText: 'Taming of the Shrew'} 
+                  ,{answerText: 'Twelfth Night'}, {answerText: 'Cymbeline'}],
+                  correctIndex: 1
+                  }
+                },
+                { QuizQuestion: {
+                  isMulti: true,
+                  id:  '',
+                  question: 'Which of the following films is directed by Anne Fletcher?',
+                  answers: [{answerText: 'tt0817230'}, {answerText: 'tt0318283'} 
+                  ,{answerText: 'tt6957966'}, {answerText: 'tt0988595'}],
+                  correctIndex: 3
+                  }
+                },
+                { QuizQuestion: {
+                  isMulti: false,
+                  id:  'tt3846674',
+                  question: 'In “To All the Boys I’ve Loved Before,” how many letters does Lara Jean write? ',
+                  answers: [{answerText: '2'}, {answerText: '5'} 
+                  ,{answerText: '17'}, {answerText: '24'}],
+                  correctIndex: 1
+                  }
+                },
+                { QuizQuestion: {
+                  isMulti: true,
+                  id:  '',
+                  question: 'Which of the following rom-com movies is loosely based on the Jane Austen novel “Emma”?',
+                  answers: [{answerText: 'tt0112697'}, {answerText: 'tt0114924'} 
+                  ,{answerText: 'tt0247638'}, {answerText: 'tt0240890'}],
+                  correctIndex: 0
+                  }
+                },
+                { QuizQuestion: {
+                  isMulti: false,
+                  id:  'tt1041829',
+                  question: 'Who starred in “The Proposal” with Ryan Reynolds?',
+                  answers: [{answerText: 'Anne Hathaway'}, {answerText: 'Kate Hudson'} 
+                  ,{answerText: 'Sandra Bullock'}, {answerText: 'Rachel Weisz'}],
+                  correctIndex: 2
+                  }
+                }
+
+              ]
+            };
+            _db.collection('Quizzes').insertOne(quiz2, function(err, res) {
               if (err) throw err
             });
         }
